d5894381c4ebd9e23588ee3713c1e54da620137f
DandaraEmiliano <emilianodandara@gmail.com>
2025-06-13 16:07:11 -0300

    Estrutura inicial do projeto api-segura

---
+++ src/main/java/com/dandaraemiliano/api_segura/ApiSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.dandaraemiliano.api_segura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSeguraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dandaraemiliano/api_segura/ApiSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dandaraemiliano.api_segura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
